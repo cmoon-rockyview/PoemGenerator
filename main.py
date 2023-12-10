398c5adbb544ecb3339d69e9e586e4ce744766d7
cmoon-rockyview <ddagge3374@yahoo.ca>
2023-12-09 18:14:40 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.chat_models import ChatOpenAI
+chat_models = ChatOpenAI()
+
+import streamlit as st
+
+st.title("AI Poem Generator")
+theme = st.text_input('Type your poem theme here')
+
+if st.button('Generate'):
+    with st.spinner("Generating..."):
+        result = chat_models.predict("write a poem about " + theme)
+        st.write(result)
+
+
+
+
